a5de2be423a5baad7d80c6d5567e13273bd5c6b3
DavidAsmunds <davidasm14@gmail.com>
2025-10-29 22:22:45 +0000

    va

--- src/scripts.js
+++ src/scripts.js
@@ -10,14 +10,14 @@ if (artSearcher instanceof HTMLElement) {
   if (id) {
     (async () => {
       try{
-        const {image_id,title,short_description} = await searchById(id);
+        const {image_id,title,short_description,width} = await searchById(id);
         if (!image_id) {
           artSearcher.appendChild(el("p", {}, `ekkert id`));
         }
         artSearcher.appendChild(el("h3", {}, `${title}`));
         artSearcher.appendChild(
           el("img",{
-            src: `https://www.artic.edu/iiif/2/${image_id}/full/843,/0/default.jpg`,
+            src: `https://www.artic.edu/iiif/2/${image_id}/full/${width},/0/default.jpg`,
             alt: `Listaverk ${id}`,
             title: `${title}`,
             loading: "lazy"
@@ -59,8 +59,9 @@ async function searchById(id) {
     const image_id = data.image_id;
     const title = data.title;
     const short_description = data.short_description;
+    const width = data.thumbnail.width;
 
-    return {image_id,title,short_description};
+    return {image_id,title,short_description,width};
   }
   catch (err) {
     error("næst ekki að sækja gögn, id eflausti invalid",err);
